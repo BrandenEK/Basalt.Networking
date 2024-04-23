3d9db7969330123a03a81a787b4b2eb70ad5c9d6
unknown <brandenek03912@hotmail.com>
2024-04-23 11:06:22 -0400

    init: add base project files

---
+++ Basalt.Networking/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basalt.Networking;
+
+public static class Logger
+{
+    public static void Info(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(message);
+    }
+
+    public static void Warn(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(message);
+    }
+
+    public static void Error(object message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+    }
+}
---
+++ Basalt.Networking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Basalt.Networking.Old;
+
+namespace Basalt.Networking;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+#if DEBUG
+        Client c = new Client();
+        c.Connect("localhost", 8989);
+#else
+        Listener l = new(null, System.Net.IPAddress.Any, 8989);
+#endif
+        Console.WriteLine("Press any key to terminate...");
+        Console.ReadKey();
+    }
+}
