c083a7d85cdf419bfabee61a9074a9263f9de949
unknown <brandenek03912@hotmail.com>
2024-04-23 11:44:56 -0400

    client: add methods

---
+++ Basalt.Networking/Client.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Basalt.Networking;
+
+public class Client
+{
+    private readonly TcpClient _client;
+    private readonly Thread _thread;
+
+    private bool _shouldStop = false;
+
+    public string Ip { get; }
+    public int Port { get; }
+
+    public Client(string ip, int port)
+    {
+        _client = new TcpClient(ip, port);
+        _thread = StartReadThread();
+
+        Ip = ip;
+        Port = port;
+    }
+
+    public void Disconnect()
+    {
+        _client.Close();
+        _shouldStop = true;
+    }
+
+    public void Send(byte[] data)
+    {
+        _client.GetStream().Write(data, 0, data.Length);
+    }
+
+    private Thread StartReadThread()
+    {
+        Thread thread = new Thread(ReadLoop);
+        thread.IsBackground = true;
+        thread.Start();
+
+        return thread;
+    }
+
+    private void ReadLoop(object state)
+    {
+        while (!_shouldStop)
+        {
+            try
+            {
+                Logger.Info("Client: Beginning read step");
+                ReadStep();
+            }
+            catch { }
+
+            Thread.Sleep(READ_INTERVAL);
+        }
+    }
+
+    private void ReadStep()
+    {
+        if (_client.Available == 0)
+            return;
+
+        byte[] buffer = new byte[_client.Available];
+        _client.Client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+        Logger.Error($"Received: {Encoding.UTF8.GetString(buffer)}");
+    }
+
+    private const int READ_INTERVAL = 1000;
+}
--- Basalt.Networking/Program.cs
+++ Basalt.Networking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 
 namespace Basalt.Networking;
 
@@ -9,6 +11,16 @@ internal class Program
         Console.WriteLine("Hello, World!");
 
         var server = new Server(8989);
+
+        var client = new Client("192.168.1.117", 8989);
+
+        Thread.Sleep(2000);
+
+        client.Send(Encoding.UTF8.GetBytes("Sending to server"));
+
+        Thread.Sleep(2000);
+
+        server.Broadcast(Encoding.UTF8.GetBytes("Sending to client"));
 
         Console.WriteLine("Press any key to terminate...");
         Console.ReadKey();
